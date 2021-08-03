0ff899cae94b88bda8d1807f62f5701b9b848064
Sean D Gillespie <sean@mistersg.net>
2021-08-02 22:14:51 -0400

    Add some dependencies we know we'll use

--- gillespies-game-of-life.nix
+++ gillespies-game-of-life.nix
@@ -1,11 +1,17 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, base, bytestring, lib, linear, resourcet, sdl2
+, transformers, unliftio-core, vector, vulkan, vulkan-utils
+, VulkanMemoryAllocator
+}:
 mkDerivation {
   pname = "gillespies-game-of-life";
   version = "0.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    base bytestring linear resourcet sdl2 transformers unliftio-core
+    vector vulkan vulkan-utils VulkanMemoryAllocator
+  ];
   homepage = "https://github.com/sgillespie/game-of-life";
   description = "Submission for GDFG's Monthly Game Jam #8";
   license = lib.licenses.bsd3;
